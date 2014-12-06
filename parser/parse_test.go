1aae9baceee8e48525da8f56b07bf6a5ca7eb147
Wenbin Xiao <xwb1989@gmail.com>
2014-12-05 23:12:40 -0500

    first commit

---
+++ parser/parse_test.go
@@ -0,0 +1,53 @@
+// Copyright 2012, Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package sqlparser
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGen(t *testing.T) {
+	_, err := Parse("select :a from a where a in (:b)")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestParse(t *testing.T) {
+	sql := "select a from (select * from table1 where table1.a = 'tom') as t1, table2, table3 as t3, table4 left join table5 where t1.k = '1'"
+	tree, err := Parse(sql)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(String(tree))
+}
+
+func BenchmarkParse1(b *testing.B) {
+	sql := "select 'abcd', 20, 30.0, eid from a where 1=eid and name='3'"
+	for i := 0; i < b.N; i++ {
+		_, err := Parse(sql)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkParse2(b *testing.B) {
+	sql := "select aaaa, bbb, ccc, ddd, eeee, ffff, gggg, hhhh, iiii from tttt, ttt1, ttt3 where aaaa = bbbb and bbbb = cccc and dddd+1 = eeee group by fff, gggg having hhhh = iiii and iiii = jjjj order by kkkk, llll limit 3, 4"
+	for i := 0; i < b.N; i++ {
+		_, err := Parse(sql)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+type testCase struct {
+	file   string
+	lineno int
+	input  string
+	output string
+}
