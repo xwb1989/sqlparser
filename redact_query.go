4ac492b13c966595baef2a16d26b0e7a4044e34f
zr-hebo <zrhebo@gmail.com>
2018-04-13 22:44:06 +0800

    add parse of ddl on database statement
    Signed-off-by: zr-hebo <zrhebo@gmail.com>

--- redact_query.go
+++ redact_query.go
@@ -1,6 +1,6 @@
 package sqlparser
 
-import querypb "github.com/youtube/vitess/go/vt/proto/query"
+import querypb "github.com/xwb1989/sqlparser/dependency/querypb"
 
 // RedactSQLQuery returns a sql string with the params stripped out for display
 func RedactSQLQuery(sql string) (string, error) {
